c8bcfde74d914d9cac8f12f1ebcf4a69c9f5e3fe
Melissa McGrath <melissam640@gmail.com>
2024-07-08 14:47:29 -0700

    Remoted default react content and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 //App.js
-import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
@@ -15,18 +14,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <button onClick={apiCall}>Make API Call</button>
       </header>
     </div>
