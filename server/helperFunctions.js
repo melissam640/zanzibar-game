b991ac33fd5e73201aebaccab2849bd79c189036
Melissa McGrath <melissam640@gmail.com>
2024-07-18 16:52:26 -0700

    Added check for three of a kind combos

--- server/helperFunctions.js
+++ server/helperFunctions.js
@@ -10,59 +10,33 @@ const rollDice = () => {
 const getPointValue = (num) => {
   if (num === 1) {
     points = 100;
-  }
-  else if (num === 6) {
+  } else if (num === 6) {
     points = 60;
-  }
-  else {
+  } else {
     points = num;
   }
   return points;
 }
 
 const getTotalScore = (diceValues) => {
+  // scores for different three-of-a-kind combinations
+  const threeComboScores = {1: 7000, 2: 6000, 3: 5000, 4: 4000, 5: 3000, 6: 2000};
+
+  // check for special combo [4, 5, 6], three-of-a-kind, or [1, 2, 3]
+  // else calculate individual points
   if (diceValues.includes(4) && diceValues.includes(5) && diceValues.includes(6)) {
     score = 8000;
     tokensExchanged = 4;
     message = 'Zanzibar!';
-  }
-// TODO: revise three-of-a-kind checks
-//   else if (diceValues == [1, 1, 1]) {
-//     score = 7000;
-//     tokensExchanged = 3;
-//     message = 'This is three of a kind!';
-//   }
-//   else if (diceValues == [2, 2, 2]) {
-//     score = 6000;
-//     tokensExchanged = 3;
-//     message = 'This is three of a kind!';
-//   }
-//   else if (diceValues == [3, 3, 3]) {
-//     score = 5000;
-//     tokensExchanged = 3;
-//     message = 'This is three of a kind!';
-//   }
-//   else if (diceValues == [4, 4, 4]) {
-//     score = 4000;
-//     tokensExchanged = 3;
-//     message = 'This is three of a kind!';
-//   }
-//   else if (diceValues == [5, 5, 5]) {
-//     score = 3000;
-//     tokensExchanged = 3;
-//     message = 'This is three of a kind!';
-//   }
-//   else if (diceValues == [6, 6, 6]) {
-//     score = 2000;
-//     tokensExchanged = 3;
-//     message = 'This is three of a kind!';
-//   }
-  else if (diceValues.includes(1) && diceValues.includes(2) && diceValues.includes(3)) {
+  } else if (diceValues.every(val => val === diceValues[0])) {
+    score = threeComboScores[diceValues[0]];
+    tokensExchanged = 3;
+    message = 'This is three of a kind!';
+  } else if (diceValues.includes(1) && diceValues.includes(2) && diceValues.includes(3)) {
     score = 1000;
     tokensExchanged = 2;
     message = 'This is a special combination!';
-  }
-  else {
+  } else {
     score = getPointValue(diceValues[0]) + getPointValue(diceValues[1]) + getPointValue(diceValues[2]);
     tokensExchanged = 1;
     message = `This is worth ${score} points!`;
@@ -72,6 +46,5 @@ const getTotalScore = (diceValues) => {
 
 module.exports = {
   rollDice,
-  getPointValue,
   getTotalScore,
 };
