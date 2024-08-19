6e4484e96b82f0582fca8233544b3a1ffefca085
Melissa McGrath <melissam640@gmail.com>
2024-08-19 16:40:39 -0700

    Added styling to modal to match other components

--- client/src/components/GameRules.js
+++ client/src/components/GameRules.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import './GameRules.css';
 
 function GameRules() {
   const [show, setShow] = useState(true);
@@ -8,46 +9,47 @@ function GameRules() {
   const handleClose = () => setShow(false);
 //   const handleShow = () => setShow(true);
 
+  const rules = `
+  Roll the dice to try to get the higher score and get rid of your tokens.
+  
+  First player to loose all of their tokens wins!`;
+
+  const scoring = `
+  The loser receives:
+
+  4 tokens if the highest score is 4,5,6 (Zanzibar)
+
+  3 tokens if the highest score is three-of-a-kind
+
+  2 tokens if the highest score is 1,2,3
+
+  1 token if the highest score is a points total
+
+  Points System:
+  1 = 100 points
+  6 = 60 points
+  2 = 2 points
+  3 = 3 points
+  4 = 4 points
+  5 = 5 points`;
+
   return (
     <>
       {/* <Button variant="primary" onClick={handleShow}>
         Launch demo modal
       </Button> */}
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Zanzibar Dice Game</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Roll the dice to try to get the higher score and get rid of your tokens.
-
-          First player to loose all of their tokens wins!
-
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Body className="rules-modal">
+          <h2 className="rules-title">Zanzibar Dice Game</h2>
+          {rules}
           <hr/>
-          The loser receives:
-
-          4 tokens if the highest score is 4,5,6 (Zanzibar)
-
-          3 tokens if the highest score is three-of-a-kind
-
-          2 tokens if the highest score is 1,2,3
-
-          1 token if the highest score is a points total
-
-          Points System:
-          1 = 100 points
-          6 = 60 points
-          2 = 2 points
-          3 = 3 points
-          4 = 4 points
-          5 = 5 points
+          {scoring}
           <hr/>
-        </Modal.Body>
-        <Modal.Footer>
           <Button variant="primary" onClick={handleClose}>
-            Close
+            Got It!
           </Button>
-        </Modal.Footer>
+        </Modal.Body>
       </Modal>
     </>
   );
