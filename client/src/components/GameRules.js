28678f693f6d15fba48fce2fa4f5e2cc2235376d
Melissa McGrath <melissam640@gmail.com>
2024-08-19 12:33:40 -0700

    Added text explaining game rules to modal

--- client/src/components/GameRules.js
+++ client/src/components/GameRules.js
@@ -22,6 +22,26 @@ function GameRules() {
           Roll the dice to try to get the higher score and get rid of your tokens.
 
           First player to loose all of their tokens wins!
+
+          <hr/>
+          The loser receives:
+
+          4 tokens if the highest score is 4,5,6 (Zanzibar)
+
+          3 tokens if the highest score is three-of-a-kind
+
+          2 tokens if the highest score is 1,2,3
+
+          1 token if the highest score is a points total
+
+          Points System:
+          1 = 100 points
+          6 = 60 points
+          2 = 2 points
+          3 = 3 points
+          4 = 4 points
+          5 = 5 points
+          <hr/>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="primary" onClick={handleClose}>
