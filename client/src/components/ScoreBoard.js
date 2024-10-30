8758d7ac6857f410490aec5bc5e2a918ebf784fd
Melissa McGrath <melissam640@gmail.com>
2024-10-30 16:18:17 -0700

    Added token icon

--- client/src/components/ScoreBoard.js
+++ client/src/components/ScoreBoard.js
@@ -1,27 +1,26 @@
 import "./ScoreBoard.css";
+import coin from "../assets/golden-coin-icon.png";
 
 const ScoreBoard = (props) => {
     return (
       <div className="Score-board-section">
         <div className="Score-board-frame Right">
-          {/* <div className="Score-board"> */}
-            <h2 className="Player-title">
-              COMPUTER
-            </h2>
-            <p className="Tokens">
-              {props.compTokens} Tokens
-            </p>
-          {/* </div> */}
+          <h2 className="Player-title">
+            COMPUTER
+          </h2>
+          <p className="Tokens">
+            {props.compTokens}
+          </p>
+          <img className="Coin-icon" src={coin} />
         </div>
         <div className="Score-board-frame Left">
-        {/* <div className="Score-board"> */}
           <h2 className="Player-title">
             YOU
           </h2>
           <p className="Tokens">
-            {props.userTokens} Tokens
+            {props.userTokens}
           </p>
-        {/* </div> */}
+          <img className="Coin-icon" src={coin} />
       </div>
     </div>
     );
