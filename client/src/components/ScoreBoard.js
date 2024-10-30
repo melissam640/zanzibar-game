aacc7ed97ab82d2f75591eabbda5f2f48a0111a1
Melissa McGrath <melissam640@gmail.com>
2024-10-30 15:12:00 -0700

    Removed wood texture and added boarder to score boards

--- client/src/components/ScoreBoard.js
+++ client/src/components/ScoreBoard.js
@@ -4,24 +4,24 @@ const ScoreBoard = (props) => {
     return (
       <div className="Score-board-section">
         <div className="Score-board-frame Right">
-          <div className="Score-board">
+          {/* <div className="Score-board"> */}
             <h2 className="Player-title">
               COMPUTER
             </h2>
             <p className="Tokens">
               {props.compTokens} Tokens
             </p>
-          </div>
+          {/* </div> */}
         </div>
         <div className="Score-board-frame Left">
-        <div className="Score-board">
+        {/* <div className="Score-board"> */}
           <h2 className="Player-title">
             YOU
           </h2>
           <p className="Tokens">
             {props.userTokens} Tokens
           </p>
-        </div>
+        {/* </div> */}
       </div>
     </div>
     );
